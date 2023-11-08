1c08c90cd144ef536b13eb92ad577a22379246b6
Leandro Santiago <lsds.snf20@uea.edu.br>
2023-11-08 16:24:48 -0400

    Added click listeners on images and result feedback

--- app/src/main/java/com/example/jokenpo/MainActivity.kt
+++ app/src/main/java/com/example/jokenpo/MainActivity.kt
@@ -2,12 +2,36 @@ package com.example.jokenpo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.example.jokenpo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+
+        binding.imStone.setOnClickListener {
+            binding.tvResult.text = "Pedra!"
+        }
+
+        binding.imPaper.setOnClickListener {
+            binding.tvResult.text = "Papel!"
+        }
+
+        binding.imScissor.setOnClickListener {
+            binding.tvResult.text = "Tesoura!"
+        }
+
     }
 
 }
